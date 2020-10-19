0d0e6a8c74430b88986b3687d860b3e28c0861c6
usfbelhadj <usf.belhadj@gmail.com>
2020-10-19 19:32:45 +0100

    Testttt to yosri

--- app/api/main.py
+++ app/api/main.py
@@ -1,5 +1,10 @@
 from app import app
+from flask import Flask, render_template
 
 @app.route('/api/')
-def index():
-	return 'index'
+def inde():
+	return render_template("static/index.html")
+
+@app.route('/home')
+def index():                             
+    return render_template('/home/djo/Youlearn/YouLearn/static/index.html')
